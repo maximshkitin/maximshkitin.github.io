6596174ddb8044a313d82d38a8b4d55e03ebdaf6
Maxim Shkitin <maxim.shkitin@gmail.com>
2016-08-02 19:36:25 +0300

    unicorn contact added

---
+++ demos/unicorn/contact/js/common.js
@@ -0,0 +1,36 @@
+var list = $('[data-animate]');
+var firstLoad = $('[data-animate="loaded"]').length;
+var	needToAnimate = list.length - firstLoad;
+$(document).ready(function () {
+	var scrollTop = $(window).scrollTop();
+	list.map(function() {
+		if (scrollTop >= $(this).offset().top - $(window).height()) {
+			var cls = $(this).data('animate');
+			$(this).addClass(cls);
+		}
+	});
+	$('.to-top a').click(function(){
+		var isSafari = /safari/.test(navigator.userAgent.toLowerCase());
+		if (isSafari) {
+	 		$('body').animate({ scrollTop: 0 }, 500);
+		} 
+	    else {
+	     	$('html').animate({ scrollTop: 0 }, 500);
+	    }
+		return false;
+	});
+})
+$(window).scroll(function(){
+	if (needToAnimate > 0) {
+		var scrollTop = $(window).scrollTop();
+		list.map(function() {
+			if (scrollTop >= $(this).offset().top - $(window).height() - 10) {
+				var cls = $(this).data('animate');
+				if (!$(this).hasClass(cls)) {
+					$(this).addClass(cls);
+					needToAnimate--;
+				}
+			}
+		});
+	}
+});
