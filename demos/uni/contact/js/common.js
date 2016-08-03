38d7657ee68ff0b8877d15eca28cffcac462c43d
Maxim Shkitin <maxim.shkitin@gmail.com>
2016-08-03 17:03:14 +0300

    contact

--- demos/uni/contact/js/common.js
+++ demos/uni/contact/js/common.js
@@ -1,6 +1,11 @@
 var list = $('[data-animate]');
-var firstLoad = $('[data-animate="loaded"]').length;
-var	needToAnimate = list.length - firstLoad;
+var	needToAnimate = list.length;
+var resetForms = function() {
+	var formList = $('.form-item');
+	formList.map(function(){
+		$(this)[0].value = '';
+	});
+}
 $(document).ready(function () {
 	$('.to-top a').click(function(){
 		var isSafari = /safari/.test(navigator.userAgent.toLowerCase());
@@ -15,19 +20,17 @@ $(document).ready(function () {
 	var currentForm = 0;
 	var forms = $('.question');
 	$('.question form').submit(function(e){
+		console.log(currentForm);
 		e.preventDefault();
-		forms.map(function(i, el){
-			if($(this).hasClass('active-form')) {
-				currentForm = i;
-				return;
-			}
-		});
 		$('.active-form').removeClass('active-form');
 		forms.eq(currentForm + 1).addClass('active-form');
 		currentForm++;
 	});
 	$('.question button.button').click(function(){
-		$('.active-form').fadeOut('fast');
+		$('.active-form').removeClass('active-form');
+		resetForms();
+		forms.eq(0).addClass('active-form');
+		currentForm = 0;
 	});
 	$('.go-back a').click(function(e){
 		e.preventDefault();
@@ -46,14 +49,13 @@ $(window).bind('load', function(){
 	});
 });
 $(window).scroll(function(){
-	if (needToAnimate > 0) {
+	if (needToAnimate > $('[class*="fadeIn"]').length) {
 		var scrollTop = $(window).scrollTop();
 		list.map(function() {
 			if (scrollTop >= $(this).offset().top - $(window).height() - 10) {
 				var cls = $(this).data('animate');
 				if (!$(this).hasClass(cls)) {
 					$(this).addClass(cls);
-					needToAnimate--;
 				}
 			}
 		});
