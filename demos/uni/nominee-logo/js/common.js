5e3257b3d63590b4f9c4f25080671aeb718e661e
Maxim Shkitin <maxim.shkitin@gmail.com>
2016-08-05 11:56:35 +0300

    nominee

---
+++ demos/uni/nominee-logo/js/common.js
@@ -0,0 +1,39 @@
+var list = $('[data-animate]');
+var needToAnimate = $('[data-animate]').length;
+$(document).ready(function () {
+	var contentHeight = $('.content').outerHeight();
+	$('aside').css('height', contentHeight);
+	$('.to-top a').click(function(){
+		var isSafari = /safari/.test(navigator.userAgent.toLowerCase());
+		if (isSafari) {
+	 		$('body').animate({ scrollTop: 0 }, 500);
+		} 
+	    else {
+	     	$('html').animate({ scrollTop: 0 }, 500);
+	    }
+		return false;
+	});
+});
+$(window).bind('load', function(){
+	var scrollTop = $(window).scrollTop();
+	list.map(function() {
+		if (scrollTop >= $(this).offset().top - $(window).height()) {
+			var cls = $(this).data('animate');
+			$(this).addClass(cls);
+		}
+	});
+	$('.modal').fadeOut(1200);
+});
+$(window).scroll(function(){
+	if (needToAnimate > $('[class*="fadeIn"]').length) {
+		var scrollTop = $(window).scrollTop();
+		list.map(function() {
+			if (scrollTop >= $(this).offset().top - $(window).height() + 30) {
+				var cls = $(this).data('animate');
+				if (!$(this).hasClass(cls)) {
+					$(this).addClass(cls);
+				}
+			}
+		});
+	}
+});
