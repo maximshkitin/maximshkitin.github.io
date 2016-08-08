8111b418d03a1efba0add825958cad0a277e627b
Maxim Shkitin <maxim.shkitin@gmail.com>
2016-08-08 11:04:27 +0300

    about, gallery, company-nomination

---
+++ demos/uni/about-r/js/common.js
@@ -0,0 +1,59 @@
+var list = $('[data-animate]');
+var needToAnimate = $('[data-animate]').length;
+var deviceWidth = $(window).width();
+var topHeight = $('.top-navbar').outerHeight();
+$(document).ready(function () {
+	$('.to-top a').click(function(){
+		var isSafari = /safari/.test(navigator.userAgent.toLowerCase());
+		if (isSafari) {
+	 		$('body').animate({ scrollTop: 0 }, 500);
+		} 
+	    else {
+	     	$('html').animate({ scrollTop: 0 }, 500);
+	    }
+		return false;
+	});
+	$('.hamburger-menu').click(function(){
+		$('#main-nav').toggleClass('open');
+		$('.navbar').toggleClass('menu-open');
+	});
+});
+$(window).on('resize', function(){
+	deviceWidth = $(window).width();
+	topHeight = $('.top-navbar').outerHeight();
+});
+$(window).bind('load', function(){
+	var scrollTop = $(window).scrollTop();
+	list.map(function() {
+		if (scrollTop >= $(this).offset().top - $(window).height()) {
+			var cls = $(this).data('animate');
+			$(this).addClass(cls);
+		}
+	});
+	$('.modal').fadeOut(1200);
+});
+$(window).scroll(function(){
+	var scrollTop = $(window).scrollTop();
+	if (deviceWidth < 1199) {
+		if (scrollTop >= topHeight) {
+			$('.navbar').addClass('sticky');
+			$('.heading').addClass('has-sticky');
+		}
+		else {
+			$('.navbar').removeClass('sticky');
+			$('.heading').removeClass('has-sticky');
+		}
+	}
+
+
+	if (needToAnimate > $('[class*="fadeIn"]').length) {
+		list.map(function() {
+			if (scrollTop >= $(this).offset().top - $(window).height() + 30) {
+				var cls = $(this).data('animate');
+				if (!$(this).hasClass(cls)) {
+					$(this).addClass(cls);
+				}
+			}
+		});
+	}
+});
