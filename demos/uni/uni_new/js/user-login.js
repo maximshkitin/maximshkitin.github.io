a155a0b1aa4e0d8be2c2140a854c48954b48d4c1
Maxim Shkitin <maxim.shkitin@gmail.com>
2016-08-20 01:13:31 +0300

    -

--- demos/uni/uni_new/js/user-login.js
+++ demos/uni/uni_new/js/user-login.js
@@ -1,7 +1,6 @@
 var list = $('[data-animate]');
 var needToAnimate = $('[data-animate]').length;
 var deviceWidth = $(window).width();
-var topHeight = $('.top-navbar').outerHeight();
 var resetForms = function() {
 	var formList = $('.form-item');
 	formList.map(function(){
@@ -19,13 +18,6 @@ $(document).ready(function () {
 	    }
 		return false;
 	});
-	$('.hamburger-menu').click(function(){
-		$('#main-nav').toggleClass('open');
-		$('.navbar').toggleClass('menu-open');
-	});
-	$('.i-hover').hover(function(){
-		$(this).parent().parent().find('.hint').toggleClass('active');
-	});
 
 
 	$('.tab').click(function(){
@@ -40,10 +32,6 @@ $(document).ready(function () {
 
 
 });
-$(window).on('resize', function(){
-	deviceWidth = $(window).width();
-	topHeight = $('.top-navbar').outerHeight();
-});
 $(window).bind('load', function(){
 	var scrollTop = $(window).scrollTop();
 	list.map(function() {
@@ -56,18 +44,6 @@ $(window).bind('load', function(){
 });
 $(window).scroll(function(){
 	var scrollTop = $(window).scrollTop();
-	if (deviceWidth < 1199) {
-		if (scrollTop >= topHeight) {
-			$('.navbar').addClass('sticky');
-			$('#content').addClass('has-sticky');
-		}
-		else {
-			$('.navbar').removeClass('sticky');
-			$('#content').removeClass('has-sticky');
-		}
-	}
-
-
 	if (needToAnimate > $('[class*="fadeIn"]').length) {
 		list.map(function() {
 			if (scrollTop >= $(this).offset().top - $(window).height() + 30) {
