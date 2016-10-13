795b7f5ff8fc1e4c29954fa347db7ac34092bdbd
Maxim Shkitin <maxim.shkitin@gmail.com>
2016-10-13 20:05:47 +0300

    bug fixed

--- demos/trv_landing/js/common.js
+++ demos/trv_landing/js/common.js
@@ -1,14 +1,16 @@
+function setHeight() {
+	var types = $('#portfolio .type');
+	types.map(function(i, val) {
+		var th = $(val);
+		var imgHeight = th.closest('.clearfix').find('.img-wrapper').height();
+		th.css('height', imgHeight);
+	});
+}
 $(document).ready(function(){
-	function setHeight() {
-		var types = $('#portfolio .type');
-		types.map(function(i, val) {
-			var th = $(val);
-			var imgHeight = th.closest('.clearfix').find('.img-wrapper').height();
-			th.css('height', imgHeight);
-		});
-	}
-	setHeight();
 	$(window).resize(function(){
 		setHeight();
 	});
+});
+$(window).load(function() {
+	setHeight();
 });
