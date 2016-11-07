329c9c90b1910810dc6ec1561f17ae0748114368
Maxim Shkitin <maxim.shkitin@gmail.com>
2016-11-07 15:16:39 +0200

    video landing

--- demos/video_landing/js/common.js
+++ demos/video_landing/js/common.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	
+	$('#video-overlay').click(function(){
+		var video = $(this).parent().find('video');
+		video[0].pause();
+		video.prop('muted', false);
+		video.prop('autoplay', false);
+		video.prop('controls', true);
+		$(this).fadeOut(300);
+		video[0].currentTime = 0;
+		video[0].play();
+	});
+	var rowH = $('#we-offer .first').closest('.row').height();
+	$('#we-offer .first').css('height', rowH);
 });
